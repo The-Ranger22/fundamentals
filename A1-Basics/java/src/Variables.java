e87c64c2049f4b3921f8cedc3c0f248d0679a331
The-Ranger22 <schandingl1@nku.edu>
2020-05-30 14:57:33 -0400

    Initial commit

---
+++ A1-Basics/java/src/Variables.java
@@ -0,0 +1,36 @@
+public class Variables {
+    public static void main(){
+        /*
+        Java is a hard typed programming language, that, unlike Python, requires the data type of its variables to be expressly declared
+         */
+        int integer = 0;
+        /*
+        The anatomy of the above declaration is as follows,
+            [datatype] [variable name] = [value];
+        */
+        /*
+        The semicolon (;) at the end of the line tells the compiler that the line of code has ended, and that it should
+        move to the next line of code.
+
+        |--------------------------------------------------------------------------------------------------------------|
+        Data types are separated between two categories, primitive (int, char, double, etc.) and non-primitive (Arrays,
+        classes, and interfaces). Primitive data types that are just that, primitive. They are 'simple', the bare bone
+        blocks that all other code is built upon. Whats cool about that is the realization that no matter how complex a
+        class might seem, underneath it all is the same familiar primitive data types you've come to know.
+        |--------------------------------------------------------------------------------------------------------------|
+        Primitive Data types (eight in total)                   Non-Primitive Data Types (No limit)
+        -int (whole numbers, negative or positive)              -Integer
+        -char ('a' 'b' 'c')                                     -String
+        -double (decimal numbers, ex. 1.2)                      -Scanner
+        -boolean (Binary true or false)                         -Etc.
+        -short
+        -byte
+        -long
+        -float
+        (For more in depth explanations, visit: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html)
+        |--------------------------------------------------------------------------------------------------------------|
+         */
+
+
+    }
+}
